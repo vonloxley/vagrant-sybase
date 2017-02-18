cea51779ab51d2b4086dd7bd0da994cb1a705933
Ruslan Gainutdinov <huksley@sdot.ru>
2017-02-18 12:41:31 +0300

    Reconfiguring with UTF8 charset, creating test DB

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # vagrant-sybase
+
 Install Sybase ASE in vagrant
+
+Installs and configures ready to use developer version of Sybase ASE.
+
+  * Port: 5001
+  * SA user - username: sa, password: Sybase123
+  * Test database: test
+  * Test user - username: test, password: 123
+
+## Usage
+
+  * Copy ASE_Suite.linuxamd64.tgz into folder (you can obtain it from sap.com)
+  * run vagrant up
+
--- common.sh
+++ common.sh
@@ -5,20 +5,25 @@ if [ ! -d ASE_Suite ]; then
 	tar -xzvf ASE_Suite.linuxamd64.tgz
 fi
 
-if [ -d /vagrant/deb ]; then
-	# Install from cache
-	dpkg -i /vagrant/deb/*.deb
-else 
-	dpkg --add-architecture i386	
-	apt-get update
-	apt-get upgrade -y
-	apt-get install -y libaio1 libc6:i386 libncurses5:i386 libstdc++6:i386
-	mkdir -p /vagrant/deb
-	rm -Rf /vagrant/deb/*.deb
-	cp /var/cache/apt/archives/*.deb /vagrant/deb/
-fi
 
-cd ASE_Suite
+if [ ! -f /vagrant/.installed ]; then
+	touch /vagrant/.installed
+
+	if [ -d /vagrant/deb ]; then
+		# Install from cache
+		dpkg -i /vagrant/deb/*.deb
+	fi
+
+	if [ ! -d /vagrant/deb ]; then
+		dpkg --add-architecture i386	
+		apt-get update
+		apt-get upgrade -y
+		apt-get install -y libaio1 libc6:i386 libncurses5:i386 libstdc++6:i386
+		mkdir -p /vagrant/deb
+		rm -Rf /vagrant/deb/*.deb
+		cp /var/cache/apt/archives/*.deb /vagrant/deb/
+	fi
+fi
 
 echo "kernel.shmmax = 300000000" >> /etc/sysctl.conf
 sysctl -p
@@ -32,9 +37,9 @@ chown sybase.sybase /opt/sybase
 
 if [ ! -d /opt/sybase/ASE-16_0 ]; then
 	echo "Installing Sybase ASE"
+	cd ASE_Suite
 	su -c "./setup.bin -f ../response.txt -i silent -DAGREE_TO_SAP_LICENSE=true" sybase
 fi
-
 
 cp /opt/sybase/jConnect-16_0/classes/jconn4.jar /vagrant
 cp /vagrant/sqlsrv.res /opt/sybase/ASE-16_0/
@@ -44,14 +49,13 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 echo $MYIP sybtest >> /etc/hosts
 
 echo source ./SYBASE.sh >>/opt/sybase/.profile
-su - sybase
 
 cat <<EOF >/tmp/asestop
 shutdown
 go
 EOF
 
-isql -S SYBTEST -U sa -P Sybase123 -i /tmp/asestop
+su -l -c "isql -S SYBTEST -U sa -P Sybase123 -i /tmp/asestop" sybase
 
 rm -Rf /opt/sybase/data/*.dat
 rm -Rf /opt/sybase/interfaces
@@ -62,5 +66,35 @@ rm -Rf /opt/sybase/ASE-16_0/install/RUN_*
 rm -Rf /opt/sybase/ASE-16_0/install/*.log
 
 echo Building Sybase ASE server
-srvbuildres -r /opt/sybase/ASE-16_0/sqlsrv.res
+su -l -c "srvbuildres -r /opt/sybase/ASE-16_0/sqlsrv.res" sybase
 
+# Load charset
+su -l -c "export DSQUERY=SYBTEST; charset -Usa -PSybase123 binary.srt utf8" sybase
+
+
+# Init nocase sorting + utf8 charset, test DB and user
+cat <<EOF >/tmp/aseconf
+exec sp_configure 'default sortorder id', 52, 'utf8'
+go
+disk init name = 'user_file_test1', physname = '/opt/sybase/data/test1.dat', size = '500M'
+go
+create database test on user_file_test1
+go
+exec master..sp_dboption test, 'allow nulls by default', true
+exec master..sp_dboption test, 'trunc log on chkpt', true
+go
+use test
+exec sp_addlogin 'test', 'Test123', 'test'
+exec sp_addalias 'test', 'dbo'
+go
+shutdown
+go
+EOF
+
+# Feed config
+su -l -c "isql -S SYBTEST -U sa -P Sybase123 -i /tmp/aseconf" sybase
+
+# Run once (will quit)
+su -l -c "/opt/sybase/ASE-16_0/install/RUN_SYBTEST" sybase
+
+# Run again
